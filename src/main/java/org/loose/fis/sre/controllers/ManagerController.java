dd48fa524b603929838185e77c9aa6d1fcfbdd87
milein4u <milanaaautica@gmail.com>
2021-05-12 02:52:25 +0300

    The manager can now add products

--- src/main/java/org/loose/fis/sre/controllers/ManagerController.java
+++ src/main/java/org/loose/fis/sre/controllers/ManagerController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
@@ -12,6 +13,7 @@ import javafx.stage.Stage;
 import org.loose.fis.sre.exceptions.AccountException;
 import org.loose.fis.sre.exceptions.IncorrectPasswordException;
 import org.loose.fis.sre.model.User;
+import org.loose.fis.sre.services.ProductNameService;
 
 import java.util.Objects;
 
@@ -23,18 +25,35 @@ public class ManagerController {
     private Button logOutButton;
     @FXML
     private Button OrdersButton;
+    @FXML
+    private ChoiceBox category;
 
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField price;
 
     public void handleLogOutButtonAction() throws Exception{
         Stage window = (Stage) logOutButton.getScene().getWindow();
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("login.fxml")));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
         window.setScene(new Scene(root, 800,600));
     }
 
     public void handleViewOrdersButtonAction() throws Exception{
         Stage window = (Stage) logOutButton.getScene().getWindow();
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("viewOrdersManager.fxml")));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("viewOrdersManager.fxml"));
         window.setScene(new Scene(root, 800,600));
+    }
+    public void handleAddButtonAction() throws Exception {
+
+        ProductNameService.checkNameDoesNotAlreadyExist(name.getText());
+        ProductNameService.addName(name.getText(),category.getSelectionModel().getSelectedItem().toString() ,Integer.parseInt(price.getText()));
+
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("viewProductsManager.fxml"));
+        Stage window = (Stage) category.getScene().getWindow();
+        window.setScene(new Scene(root, 800, 600));
+
     }
     public void handleMakeOffersButtonAction() throws Exception{
 
